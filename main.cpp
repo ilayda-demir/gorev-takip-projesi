4705ff354607738a4f22fae2ad4b46bbc01512ae
ilayda-demir <ilayda.boncolo@gmail.com>
2025-04-27 21:33:59 +0300

    İlk commit

---
+++ main.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <limits>
+
+class Task{
+
+private:
+
+std::string name;
+int id;
+bool is_completed;
+
+
+public:
+
+// PARAMETRESİZ CONSTRUCTOR (ekledik)
+Task() {
+    id = 0;
+    name = "";
+    is_completed = false;
+}
+
+Task(int id_, std::string name_){
+
+name=name_;
+id=id_;
+is_completed= false;
+
+}
+
+void complete(){
+
+    is_completed= true;
+}
+
+void display(){
+
+std::cout<<"id:"<<id<<std::endl;
+std::cout<<"name:"<<name<<std::endl;
+std::cout<<"is completed?:"<<(is_completed ? "tamamlandi" : "tamamlanmadi")<<std::endl;
+
+}
+
+
+};
+
+int main(){
+
+    std::map<int, Task> mapim;
+    int counter=1;
+    int secim;
+    
+
+    do {
+
+        std::cout<<"Gorev eklemek icin 1'e, gorev listelemek icin 2'ye cikis yapmak icin 3'e, gorev tamamlamak icin 4'e, silmek için 5'e basiniz. basiniz."<<std::endl;
+
+        std::cin>> secim;
+
+        if(std::cin.fail()){
+
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize> :: max(), '\n');
+            std::cout<< "gecerli bir sayi giriniz!"<< std::endl;
+            continue;}
+
+        else if(secim==1){
+
+            std::string name_;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+
+            std::cout<< "gorev adi giriniz."<< std::endl;
+
+            std::getline(std::cin, name_);
+
+            Task gorev(counter, name_);
+
+
+            mapim[counter]= gorev;
+
+            counter++;
+
+        }
+
+        else if(secim==2){
+
+            for(auto& index: mapim){
+
+                index.second.display();
+
+            }
+        }
+
+        else if(secim==3){
+
+            std::cout<< "programdan cikiliyor."<< std::endl;
+
+            break;
+
+        }else if(secim==4){
+            int idmm;
+
+            std::cout<< "Tamamlanacak gorevin idsini giriniz."<< std::endl;
+            std::cin>>idmm;
+
+            if(mapim.find(idmm) != mapim.end()){
+
+                mapim[idmm].complete();
+                std::cout << "Gorev tamamlandi!" << std::endl;
+            }else{
+
+                std::cout << "Bu ID'ye sahip bir gorev bulunamadi!" << std::endl;
+            }}
+
+            else if(secim==5){
+                int idmmm;
+    
+                std::cout<< "Siliencek gorevin idsini giriniz."<< std::endl;
+                std::cin>>idmmm;
+    
+                if(mapim.find(idmmm) != mapim.end()){
+    
+                    mapim.erase(idmmm);
+                    std::cout << "Gorev silindi!" << std::endl;
+                }else{
+    
+                    std::cout << "Bu ID'ye sahip bir gorev bulunamadi!" << std::endl;
+                }}
+
+        else {
+            std::cout << "Gecerli bir secim yapmadiniz." << std::endl;
+        }
+
+
+
+        }while(true);
+
+   
+
+    return 0;
+}
