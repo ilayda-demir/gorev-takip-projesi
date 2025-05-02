cb4f6e7c785fb7f87f1b9a2ad97d267d3b8c3487
ilayda-demir <ilayda.boncolo@gmail.com>
2025-05-02 22:34:18 +0300

    Refactor: Use shared_ptr for Task memory management

--- main.cpp
+++ main.cpp
@@ -1,262 +1,188 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <memory> // 💡 smart pointer için şart
 #include <limits>
 #include <vector>
 #include <algorithm>
 
+enum class Oncelik { Dusuk, Orta, Yuksek };
 
-enum class Oncelik {Dusuk, Orta, Yuksek};
-
-class Task{
-
+class Task {
 private:
-
-std::string name;
-int id;
-bool is_completed;
-Oncelik onem;
-
-
+    std::string name;
+    int id;
+    bool is_completed;
+    Oncelik onem;
 
 public:
-
-// PARAMETRESİZ CONSTRUCTOR (ekledik)
-Task() {
-    id = 0;
-    name = "";
-    is_completed = false;
-}
-
-Task(int id_, std::string name_, Oncelik onem_){
-
-name=name_;
-id=id_;
-is_completed= false;
-onem= onem_;
-
-}
-
-void complete(){
-
-    is_completed= true;
-}
-
-bool isCompleted() const {
-    return is_completed;
-}
-
-
-void  display() const{
-
-std::cout<<"id:"<<id<<std::endl;
-std::cout<<"name:"<<name<<std::endl;
-std::cout<<"is completed?:"<<(is_completed ? "tamamlandi" : "tamamlanmadi")<<std::endl;
-std::cout<<"Oncelik:";
-
-switch(onem){
-
-    case Oncelik::Dusuk : std::cout << "Dusuk"; break;
-    case Oncelik::Orta: std::cout << "Orta"; break;
-    case Oncelik::Yuksek: std::cout << "Yuksek"; break;
-}
-std::cout << std::endl;
-}
-
-
-
-std::string  getName() const{
-
-    return name;
-}};
-
-void findTaskByName( const std::map<int, Task>& gorevler, const std::string& arananisim ){
-
-bool bulundu=false;
-
-for(const auto& eleman: gorevler){
-    const Task& gorev = eleman.second; 
-
-    if(gorev.getName()== arananisim){
-        
-        gorev.display();
-        bulundu=true;
-
+    Task() {
+        id = 0;
+        name = "";
+        is_completed = false;
     }
 
-    if(!bulundu){
+    Task(int id_, std::string name_, Oncelik onem_) {
+        name = name_;
+        id = id_;
+        is_completed = false;
+        onem = onem_;
+    }
 
+    void complete() {
+        is_completed = true;
+    }
+
+    bool isCompleted() const {
+        return is_completed;
+    }
+
+    std::string getName() const {
+        return name;
+    }
+
+    void display() const {
+        std::cout << "id: " << id << std::endl;
+        std::cout << "name: " << name << std::endl;
+        std::cout << "is completed?: " << (is_completed ? "tamamlandi" : "tamamlanmadi") << std::endl;
+        std::cout << "Oncelik: ";
+        switch (onem) {
+            case Oncelik::Dusuk: std::cout << "Dusuk"; break;
+            case Oncelik::Orta: std::cout << "Orta"; break;
+            case Oncelik::Yuksek: std::cout << "Yuksek"; break;
+        }
+        std::cout << std::endl;
+    }
+};
+
+void findTaskByName(const std::map<int, std::shared_ptr<Task>>& gorevler, const std::string& arananisim) {
+    bool bulundu = false;
+    for (const auto& eleman : gorevler) {
+        const auto& gorev = eleman.second;
+        if (gorev->getName() == arananisim) {
+            gorev->display();
+            bulundu = true;
+        }
+    }
+    if (!bulundu) {
         std::cout << "Bu isimde bir gorev bulunamadi." << std::endl;
     }
-
-
 }
 
-
-}
-
-int sayCompletedTasks(const std::map<int, Task>& gorevler) {
+int sayCompletedTasks(const std::map<int, std::shared_ptr<Task>>& gorevler) {
     return std::count_if(gorevler.begin(), gorevler.end(), [](const auto& eleman) {
-        const Task& g = eleman.second;
-        return g.isCompleted(); // is_completed kontrolü için getter lazım!
+        return eleman.second->isCompleted();
     });
 }
 
-
-void listeleIsmeGore(const std::map<int, Task>& gorevler){
-
-
-    std::vector<Task> kopya;
-
+void listeleIsmeGore(const std::map<int, std::shared_ptr<Task>>& gorevler) {
+    std::vector<std::shared_ptr<Task>> kopya;
     for (const auto& eleman : gorevler) {
         kopya.push_back(eleman.second);
     }
 
-    std::sort(kopya.begin(), kopya.end(), [](const Task& a, const Task& b) {
-        return a.getName() < b.getName();
+    std::sort(kopya.begin(), kopya.end(), [](const std::shared_ptr<Task>& a, const std::shared_ptr<Task>& b) {
+        return a->getName() < b->getName();
     });
 
     std::cout << "--- İsme Göre Sıralı Görevler ---" << std::endl;
     for (const auto& g : kopya) {
-        g.display();
+        g->display();
     }
-
-
 }
 
-
-
-
-int main(){
-
-    std::map<int, Task> mapim;
-    int counter=1;
+int main() {
+    std::map<int, std::shared_ptr<Task>> mapim;
+    int counter = 1;
     int secim;
-    
 
     do {
+        std::cout << "\n1: Gorev Ekle\n2: Listele\n3: Cikis\n4: Gorev Tamamla\n5: Gorev Sil\n6: Isimle Ara\n7: İsme Gore Sirala\n8: Tamamlanan Sayisi\nSeciminiz: ";
+        std::cin >> secim;
 
-        std::cout<<"Gorev eklemek icin 1'e, gorev listelemek icin 2'ye cikis yapmak icin 3'e, gorev tamamlamak icin 4'e, silmek icin 5'e, gorev aramak icin 6'ya, gorevleri isime gore siralamak icin 7'ye, tamamlanan gorev sayisi icin 8'e    basiniz."<<std::endl;
-
-        std::cin>> secim;
-
-        if(std::cin.fail()){
-
+        if (std::cin.fail()) {
             std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize> :: max(), '\n');
-            std::cout<< "gecerli bir sayi giriniz!"<< std::endl;
-            continue;}
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Gecerli bir sayi giriniz!" << std::endl;
+            continue;
+        }
 
-        else if(secim==1){
-
+        if (secim == 1) {
             std::string name_;
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            
-
-            std::cout<< "gorev adi giriniz."<< std::endl;
-
+            std::cout << "Gorev adi giriniz: ";
             std::getline(std::cin, name_);
-
-           
 
             int oncelikSecim;
             std::cout << "Oncelik secin (0: Dusuk, 1: Orta, 2: Yuksek): ";
             std::cin >> oncelikSecim;
 
-            
             Oncelik oncelik;
             if (oncelikSecim == 0) oncelik = Oncelik::Dusuk;
             else if (oncelikSecim == 1) oncelik = Oncelik::Orta;
             else oncelik = Oncelik::Yuksek;
 
-            Task gorev(counter, name_, oncelik);
-
-
-            mapim[counter]= gorev;
-
+            auto gorev = std::make_shared<Task>(counter, name_, oncelik);
+            mapim[counter] = gorev;
             counter++;
-
         }
 
-        else if(secim==2){
-
-            for(auto& index: mapim){
-
-                index.second.display();
-
+        else if (secim == 2) {
+            for (auto& index : mapim) {
+                index.second->display();
             }
         }
 
-        else if(secim==3){
-
-            std::cout<< "programdan cikiliyor."<< std::endl;
-
+        else if (secim == 3) {
+            std::cout << "Programdan cikiliyor." << std::endl;
             break;
+        }
 
-        }else if(secim==4){
-            int idmm;
-
-            std::cout<< "Tamamlanacak gorevin idsini giriniz."<< std::endl;
-            std::cin>>idmm;
-
-            if(mapim.find(idmm) != mapim.end()){
-
-                mapim[idmm].complete();
+        else if (secim == 4) {
+            int id;
+            std::cout << "Tamamlanacak gorevin ID'si: ";
+            std::cin >> id;
+            if (mapim.find(id) != mapim.end()) {
+                mapim[id]->complete();
                 std::cout << "Gorev tamamlandi!" << std::endl;
-            }else{
-
+            } else {
                 std::cout << "Bu ID'ye sahip bir gorev bulunamadi!" << std::endl;
-            }}
+            }
+        }
 
-            else if(secim==5){
-                int idmmm;
-    
-                std::cout<< "Siliencek gorevin idsini giriniz."<< std::endl;
-                std::cin>>idmmm;
-    
-                if(mapim.find(idmmm) != mapim.end()){
-    
-                    mapim.erase(idmmm);
-                    std::cout << "Gorev silindi!" << std::endl;
-                }else{
-    
-                    std::cout << "Bu ID'ye sahip bir gorev bulunamadi!" << std::endl;
-                }}
+        else if (secim == 5) {
+            int id;
+            std::cout << "Silinecek gorevin ID'si: ";
+            std::cin >> id;
+            if (mapim.erase(id)) {
+                std::cout << "Gorev silindi!" << std::endl;
+            } else {
+                std::cout << "Bu ID'ye sahip bir gorev bulunamadi!" << std::endl;
+            }
+        }
 
-        else if(secim==6){
-
+        else if (secim == 6) {
             std::string isim;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize> :: max(), '\n');
-            std::cout<< "Gorev adi giriniz."<< std::endl;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Gorev adi giriniz: ";
             std::getline(std::cin, isim);
             findTaskByName(mapim, isim);
-             
-
-
-            }
+        }
 
         else if (secim == 7) {
             listeleIsmeGore(mapim);
-            }
+        }
 
         else if (secim == 8) {
-    int adet = sayCompletedTasks(mapim);
-    std::cout << "Tamamlanan gorev sayisi: " << adet << std::endl;
-    }
-    
-
-            
+            int adet = sayCompletedTasks(mapim);
+            std::cout << "Tamamlanan gorev sayisi: " << adet << std::endl;
+        }
 
         else {
             std::cout << "Gecerli bir secim yapmadiniz." << std::endl;
         }
 
-
-
-        }while(true);
-
-   
+    } while (true);
 
     return 0;
 }
